903ff2966305f7f81698876503e2902e20f4ab02
Cristian Saball <c.saball@externo.ibermatica.com>
2021-01-16 12:02:03 +0100

    pipe propio activar desactivar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre:string ='Capitán América';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  activar :boolean = true;
+  
+  PI:number =Math.PI;
+
+  heroe={
+    nombre:'Logan',
+    clave:'Wolverine',
+    edad:500,
+    direccion:{
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
+  valorPromesa = new Promise<string>((resolve)=>{
+  
+    setTimeout(()=>{
+    resolve('llega el dato');
+  },4500);
+  });
 }
